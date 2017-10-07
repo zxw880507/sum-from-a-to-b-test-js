49c38b07167699861ef903d78838c03690fcad92
Sam Meech-Ward <sam@meech-ward.com>
2017-10-06 18:56:45 -0700

    make the tests pass

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN == toN) {
+      return toN;
+  }
+
+  return fromN + sum(fromN+1, toN);
 }
 
 module.exports = sum;
